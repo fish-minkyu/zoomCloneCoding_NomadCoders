ab20df83e4b938ef22694e63c075981fc72071cb
fish-minkyu <e951219@naver.com>
2023-11-26 23:00:05 +0900

    create: 2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-// server.js (express server + ws server)
+
 import express from "express"
 import http from "http" 
-import WebSocket from "ws"
+import SocketIO from "socket.io"
 
 const app = express()
 
@@ -14,54 +14,14 @@ app.get("/*", (req, res) => res.redirect("/"))
 const handleListen = () => console.log("http://localhost:3000")
 
 // http 서버
-const server = http.createServer(app) 
+const httpServer = http.createServer(app) 
 
-// webSocket 서버
-const wss = new WebSocket.Server({ server }) // server 전달
+// SocketIO 서버(HTTP 위에 쌓아 올려서 만듬)
+const io = SocketIO(httpServer)
 
-// Fake Database 만들기
-// 누군가 우리 서버에 연결하면 그 connection을 넣을 것이다.
-// 이렇게 하면 받은 메시지를 다른 모든 socket에 전달할 수 있다.
-const sockets = []
+// frontend-backend SocketIO 연결
+io.on("connection", socket => {
+  console.log(socket)
+})
 
-
-// connection (on method)
-// connection이 생겼을 때 socket으로 즉시 메시지 보내기
-wss.on('connection', (socket) => {
-  // 서버에 연결된 브라우저를 sockets 배열에 넣어주기
-  sockets.push(socket)
-
-  // socket이 연결될 때, 익명 닉네임 생성
-  socket["nickname"] = "Anonymous"
-
-  console.log(`Connect to Browser ✅`);
-
-  // 브라우저의 연결이 끊긴 close 이벤트 listen, 콘솔 출력
-  socket.on('close', () => {
-    console.log('Disconnected from the browser ❌')
-  });
-
-  // back-end에서 socket으로 메시지를 전송하고 싶으면
-  // object를 가지고 와서 string으로 만들어줘야 한다.
-  socket.on('message', (msg) => { /* message는 string이다.*/
-    // front에서 받은 string을 javasript object로 변환
-    const message = JSON.parse(msg)
-
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach(aSocket => {
-          // Buffer를 문자열로 반환
-          const decodedMessage = message.payload.toString('utf-8')
-          // nickname property를 socket.object에 저장하고 있다.
-          aSocket.send(`${socket.nickname}: ${decodedMessage}`)
-        })
-      
-      case "nickname":
-       // socket은 객체이므로 원하는 속성 추가
-       socket["nickname"] = message.payload;
-    };
-  });
-});
-
-
-server.listen(3000, handleListen)
+httpServer.listen(3000, handleListen)
